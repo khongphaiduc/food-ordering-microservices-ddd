fc921589bfa6fd977a241327e689547ad94839ba
PhamTrungDuc <ptrungduc1011@gmail.com>
2026-03-16 11:22:04 +0700

    tracking v1

---
+++ tracking-service/Tracking.Domain/Repository/IUserSessionRepository.cs
@@ -0,0 +1,16 @@
+﻿using tracking_service.Tracking.Domain.Aggregate;
+using tracking_service.Tracking.Domain.Entities;
+
+namespace tracking_service.Tracking.Domain.Repository
+{
+    public interface IUserSessionRepository
+    {
+        Task<bool> AddNewUserSession(UserSessionAggregate sessionAggregate);
+
+
+        Task<bool> AddEvent(TrackingEvent trackingEvent);
+
+        Task<bool> SessionExists(Guid sessionId);
+
+    }
+}
